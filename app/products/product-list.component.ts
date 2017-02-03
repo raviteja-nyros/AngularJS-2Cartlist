2a5c8088cc5cea62f682ca6a71c055b9302ac547
Ravi <raviteja_nyros@yahoo.com>
2017-02-03 14:52:51 +0530

    autoload on scroll

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { InfiniteScroll } from 'angular2-infinite-scroll';
+
+
 @Component({
   moduleId: module.id, //relative paths with moduleId, where templateUrl/styleUrls get app/products automatically
   templateUrl: 'product-list.html',
+  providers: [ InfiniteScroll ],
   styleUrls: ['product-list.component.css']
 })
+
 
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
@@ -16,6 +21,7 @@ export class ProductListComponent implements OnInit {
   listFilter: string;
   products: IProduct[];
   errorMessage: string;
+  data:array = [];
 
   constructor(private _productService: ProductService) {
 
@@ -26,12 +32,27 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
     this._productService.getProducts()
       .subscribe(products => this.products = products,
+        data =>this.products.slice(0,5) = data,
         error => this.errorMessage = <any>error);
+
   }
 
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List' + message;
   }
+
+
+  onScrollDown () {
+    //console.log(typeof(this.products))
+    // console.log(this.products.slice(0, 5))
+    this.data = this.products.slice(0, this.data.length + 5);
+    console.log(this.data); 
+  }
+  onScrollUp () {
+      console.log('scrolled up!!')
+  }
+
 }
