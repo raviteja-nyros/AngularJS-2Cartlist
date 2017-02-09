15e85c02fcc4a09ff225ffecb279eeb6c20669c7
Ravi <raviteja_nyros@yahoo.com>
2017-02-09 14:57:18 +0530

    intial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,15 +6,13 @@ import { ProductService } from './products/product.service';
 @Component({
   selector: 'pm-app',
   template:`
-              <header></header>
-              <div class='container-fluid'>
-                  <router-outlet ></router-outlet> 
-              </div>
+            <header></header>
+            <div class='container-fluid'>
+                <router-outlet ></router-outlet> 
+            </div>
             `,
   styles: ['.logo-dimensions{ width:"25px";height:"25px" } '],
   providers: [ProductService],
-  styleUrls: ['style.css']
-
 })
 
 export class AppComponent {
